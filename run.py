bbfa8babdc97406d1f9f98139bd6b56991708bf0
Hemanth Tirukovalluru <124372041+Hemanth098@users.noreply.github.com>
2026-05-13 14:46:59 +0530

    Update run.py

--- run.py
+++ run.py
@@ -55,7 +55,7 @@ def seed_db():
         db.session.add(t)
 
     db.session.commit()
-
+    print("SUCCESSFULLY CREATED")
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
